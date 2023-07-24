a1686f21c683a56ec4949803cc02bad52ee34e6c
Lucas <lermenlucas@outlook.com>
2023-07-24 18:37:32 -0300

    Inserindo anotação do CORS

--- src/main/java/com/example/help_desk/repositories/SupportRepository.java
+++ src/main/java/com/example/help_desk/repositories/SupportRepository.java
@@ -4,7 +4,9 @@ import com.example.help_desk.model.InLineEmployee;
 import com.example.help_desk.model.Support;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins ="*")
 @RepositoryRestResource(excerptProjection = InLineEmployee.class)
 public interface SupportRepository extends JpaRepository<Support, Long> {
 }
